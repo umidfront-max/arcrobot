63256ec2a76f4da9360a420c2948c4e4acbb3b6f
umidfront-max <umid.front@gmail.com>
2025-09-05 22:40:35 +0500

    fix: item

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -2,34 +2,41 @@ import React, { useEffect, useState } from "react";
 import CatalogCard from "./CatalogCard";
 import { getCatalog_API } from "../service/index";
 
-
 const Solutions = () => {
-         const [catalog, setCatalog] = useState([]);
-      
-         useEffect(() => {
-            fetchCatalog();
-         }, []);
-      
-         const fetchCatalog = async () => {
-            const [error, response] = await getCatalog_API();
-            if (error) {
-               console.error("Xatolik:", error);
-               return;
-            }
-            setCatalog(response.data);
-         };
+	const [catalog, setCatalog] = useState([]);
+
+	useEffect(() => {
+		fetchCatalog();
+	}, []);
+
+	const fetchCatalog = async () => {
+		const [error, response] = await getCatalog_API();
+		if (error) {
+			console.error("Xatolik:", error);
+			return;
+		}
+		setCatalog(response.data);
+	};
 	return (
 		<div className="mt-4 max-sm:mt-12">
-			<h2 className="text-[140px] max-md:text-[100px] max-sm:text-[62px] font-inter-600 text-center">Решения</h2>
+			<h2 className="text-[140px] max-md:text-[100px] max-sm:text-[62px] font-inter-600 text-center">
+				Решения
+			</h2>
 			<p className="text-center -mt-4 max-sm:mt-0 text-gray/60">
 				Готовые роботизированные <br /> ячейки под разные задачи{" "}
 			</p>
 			<section className="py-20" id="projects">
 				<div className="container1 mx-auto">
 					<div className="grid lg:grid-cols-2 gap-2">
-						{catalog.slice(0,2).map((project, index) => (
-							<CatalogCard key={index} project={project} index={index} />
-						))}
+						{catalog
+							.filter((el) => el.isHome)
+							.map((project, index) => (
+								<CatalogCard
+									key={index}
+									project={project}
+									index={index}
+								/>
+							))}
 					</div>
 				</div>
 			</section>
